a6d8f2f6d3b2cd052b524852fd64e555ade71fbb
Caio-vi <84972740+Caio-vi@users.noreply.github.com>
2025-11-06 18:19:41 -0300

    Refactor image arrays and correct function logic

--- js/script.js
+++ js/script.js
@@ -1,67 +1,68 @@
- 
-const indexfotos = [
- 'imagens/Avelar Fotografia-1622.jpg ', 'imagens/Avelar Fotografia-1795.jpg ', 'imagens/Avelar Fotografia-1817.jpg'
+ const indexfotos = [
+    'imagens/Avelar Fotografia-1622.jpg',
+    'imagens/Avelar Fotografia-1795.jpg',
+    'imagens/Avelar Fotografia-1817.jpg'
 ];
 
 const sobrefotos = [
-    '../imagens/Avelar Fotografia-1622.jpg',
-    '../imagens/Avelar Fotografia-1795.jpg', 
-    '../imagens/Avelar Fotografia-1817.jpg',
-    '../imagens/Avelar Fotografia-3652.jpg',
-    '../imagens/Avelar Fotografia-3660.jpg',
-    '../imagens/Avelar Fotografia-3673 copy.jpg',
-    '../imagens/Avelar Fotografia-3673.jpg',
-    '../imagens/Avelar Fotografia-3685.jpg',
-    '../imagens/Estúdio Avellar-7378.jpg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.20 (1).jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.20 (2).jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.20.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.21 (1).jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.21.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.22 (1).jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.22.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.23 (1).jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.23 (2).jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.23.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.24 (1).jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.24.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.25 (1).jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.25 (2).jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.25.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.26.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.42.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.43.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.45.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.46.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.47.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.48.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.49.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.50.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.51.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.52.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.53 (1).jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.53 (2).jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.53.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.55 (1).jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.55.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.56.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.57.jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.58 (1).jpeg',
-    '../imagens/WhatsApp Image 2025-10-16 at 19.23.58.jpeg',
-    '../imagens/WhatsApp Image 2025-10-25 at 18.41.40 (1).jpeg',
-    '../imagens/WhatsApp Image 2025-10-25 at 18.41.40 (2).jpeg',
-    '../imagens/WhatsApp Image 2025-10-25 at 18.41.40 (3).jpeg',
-    '../imagens/WhatsApp Image 2025-10-25 at 18.41.40.jpeg',
-    '../imagens/WhatsApp Image 2025-10-25 at 18.41.41 (1).jpeg',
-    '../imagens/WhatsApp Image 2025-10-25 at 18.41.41 (2).jpeg',
-    '../imagens/WhatsApp Image 2025-10-25 at 18.41.41 (3).jpeg',
-    '../imagens/WhatsApp Image 2025-10-25 at 18.41.41 (4).jpeg',
-    '../imagens/WhatsApp Image 2025-10-25 at 18.41.41.jpeg',
-    '../imagens/WhatsApp Image 2025-10-25 at 18.42.12 (1).jpeg',
-    '../imagens/WhatsApp Image 2025-10-25 at 18.42.12 (2).jpeg',
-    '../imagens/WhatsApp Image 2025-10-25 at 18.42.12.jpeg',
-    '../imagens/WhatsApp Image 2025-10-25 at 18.42.13 (1).jpeg',
-    '../imagens/WhatsApp Image 2025-10-25 at 18.42.13.jpeg'
+    'imagens/Avelar Fotografia-1622.jpg',
+    'imagens/Avelar Fotografia-1795.jpg', 
+    'imagens/Avelar Fotografia-1817.jpg',
+    'imagens/Avelar Fotografia-3652.jpg',
+    'imagens/Avelar Fotografia-3660.jpg',
+    'imagens/Avelar Fotografia-3673 copy.jpg',
+    'imagens/Avelar Fotografia-3673.jpg',
+    'imagens/Avelar Fotografia-3685.jpg',
+    'imagens/Estúdio Avellar-7378.jpg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.20 (1).jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.20 (2).jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.20.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.21 (1).jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.21.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.22 (1).jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.22.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.23 (1).jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.23 (2).jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.23.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.24 (1).jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.24.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.25 (1).jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.25 (2).jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.25.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.26.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.42.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.43.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.45.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.46.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.47.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.48.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.49.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.50.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.51.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.52.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.53 (1).jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.53 (2).jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.53.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.55 (1).jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.55.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.56.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.57.jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.58 (1).jpeg',
+    'imagens/WhatsApp Image 2025-10-16 at 19.23.58.jpeg',
+    'imagens/WhatsApp Image 2025-10-25 at 18.41.40 (1).jpeg',
+    'imagens/WhatsApp Image 2025-10-25 at 18.41.40 (2).jpeg',
+    'imagens/WhatsApp Image 2025-10-25 at 18.41.40 (3).jpeg',
+    'imagens/WhatsApp Image 2025-10-25 at 18.41.40.jpeg',
+    'imagens/WhatsApp Image 2025-10-25 at 18.41.41 (1).jpeg',
+    'imagens/WhatsApp Image 2025-10-25 at 18.41.41 (2).jpeg',
+    'imagens/WhatsApp Image 2025-10-25 at 18.41.41 (3).jpeg',
+    'imagens/WhatsApp Image 2025-10-25 at 18.41.41 (4).jpeg',
+    'imagens/WhatsApp Image 2025-10-25 at 18.41.41.jpeg',
+    'imagens/WhatsApp Image 2025-10-25 at 18.42.12 (1).jpeg',
+    'imagens/WhatsApp Image 2025-10-25 at 18.42.12 (2).jpeg',
+    'imagens/WhatsApp Image 2025-10-25 at 18.42.12.jpeg',
+    'imagens/WhatsApp Image 2025-10-25 at 18.42.13 (1).jpeg',
+    'imagens/WhatsApp Image 2025-10-25 at 18.42.13.jpeg'
 ];
 
 let indiceAtual = 0;
@@ -69,7 +70,6 @@ let indiceAtual = 0;
 function proximaimagem(){
     const slides = document.getElementsByClassName("slide");
 
-    // ✅ CORREÇÃO: usa sobrefotos.length (não indexfotos.length)
     indiceAtual = (indiceAtual + 1) % sobrefotos.length;
 
     for(let i = 0; i < slides.length; i++){
@@ -84,13 +84,11 @@ function proximaimagem(){
     }
 }
 
-// ✅ APENAS UM intervalo (remove o duplicado)
 setInterval(proximaimagem, 3000);
 
 window.addEventListener('load', function() {
     const slides = document.getElementsByClassName("slide");
     for (let i = 0; i < slides.length; i++) {
-        // ✅ CORREÇÃO: usa sobrefotos[0] (não indexfotos[0])
         slides[i].src = sobrefotos[0];
     }
 });
@@ -98,8 +96,8 @@ window.addEventListener('load', function() {
 function agendar() {
   window.location.href = "agendar.html";
 }
+
 function agendarWhatsApp(servico) {
-  // Objeto com mensagens para cada serviço
   const mensagens = {
     consulta: "Olá! Gostaria de agendar uma consulta geral.",
     cirurgia: "Olá! Gostaria de um orçamento para cirurgia.",
@@ -113,21 +111,18 @@ function agendarWhatsApp(servico) {
     "urina-fezes": "Olá! Gostaria de saber mais sobre exames de urina e fezes.",
   };
 
-  // Pega a mensagem correta baseada no serviço clicado
   const mensagem = mensagens[servico];
   const numero = "5535998181384";
 
-  // Cria a URL do WhatsApp
-  const url = `https://wa.me/${"5535998181384"}?text=${encodeURIComponent(
-    mensagem
-  )}`;
+  const url = `https://wa.me/${"5535998181384"}?text=${encodeURIComponent(mensagem)}`;
 
-  // Abre em nova aba
   window.open(url, "_blank");
 }
+
 function sobre() {
-  window.location.href = "Sobre.html";
+  window.location.href = "sobre.html";
 }
+
 function contatos() {
-  window.location.href = "Contato.html";
+  window.location.href = "contato.html";
 }
